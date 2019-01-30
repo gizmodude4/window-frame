2b37c94dfd5607b5bc496a9cf0137867a161adc7
Colby Sites <colby.sites@gmail.com>
2019-01-29 23:46:16 -0600

    Adding config file support.

--- main2.py
+++ main2.py
@@ -1,54 +1,113 @@
-import os
-import pyglet
+#!/usr/bin/env python
+import argparse
+import errno
+import json
 import pydub
-import threading
+import pyglet
+import sys
 
 from pyglet.gl import *
 from pydub.playback import play
 
 
-def load_images(path, duration):
-    images = []
-    files = os.listdir(path)
-    frame_duration = duration / float(len(files))
-    for file_name in files:
-        frame = pyglet.image.AnimationFrame(path + os.sep + file_name, frame_duration)
-        images.append(frame)
-    return images
+class Window:
+  def __init__(self, image, music):
+    self.image = image
+    self.music = music
+    self.effects = []
+
+  def add_effect(self, effect):
+    self.effects.append(effect)
 
 
-def load_song(path):
-    audio = pydub.AudioSegment.from_file(path)
-    audio.fade_out(3000)
-    return audio
+class Music:
+  def __init__(self, songs, fade_in, fade_out):
+    self.songs = songs
+    self.fade_in = fade_in
+    self.fade_out = fade_out
 
 
-def load_animation_from_path(path, size_x, size_y):
-    images = []
-    for file_name in os.listdir(path):
-        image = pygletpygame.image.load(path + os.sep + file_name).convert()
-        image = pygame.transform.scale(image, (screen_info.current_w, screen_info.current_h))
-        images.append(image)
-    return images
+class Effect:
+  def __init__(self, name, image, audio_path, audio_strength):
+    self.name = name
+    self.image = image
+    self.audio_path = audio_path
+    self.audio_strength = audio_strength
+
+
+def parse_config(config_path):
+  with open(config_path, 'r') as stream:
+    config = json.load(stream)
+    windows = []
+    for window_config in config["windows"]:
+      music_config = window_config['music']
+      music = Music(music_config['songs'], music_config['fadeIn'], music_config['fadeOut'])
+      window = Window(window_config['image'], music)
+      for effect in window_config['effects']:
+        effect = Effect(effect['name'], effect['image'],
+                        effect['overlayAudio']['audio'],
+                        effect['overlayAudio']['volume'])
+        window.add_effect(effect)
+      windows.append(window)
+  return windows
+
+
+def load_songs(music):
+  playlist = pydub.AudioSegment.empty()
+  for song in music['songs']:
+    audio = pydub.AudioSegment.from_file(song)
+    if 'fadeIn' in music:
+      audio.fade_in(music['fadeIn'])
+    if 'fadeOut' in music:
+      audio.fade_in(music['fadeOut'])
+    playlist += audio
+  return playlist
 
 
 def main():
+    r""" This script runs the whole shebang."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True,
+                        help="config file")
     glEnable(GL_TEXTURE_2D)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    window = pyglet.window.Window(fullscreen=True)
+    display = pyglet.window.Window(fullscreen=True)
+
+    args = parser.parse_args()
+    current_window_index = 0
+    windows = parse_config(args.config)
+    current_window = windows[current_window_index]
     
-    animation = pyglet.resource.animation('assets/landscape.gif')
+    animation = pyglet.resource.animation(current_window.image)
     bin = pyglet.image.atlas.TextureBin()
     animation.add_to_texture_bin(bin)
     sprite = pyglet.sprite.Sprite(animation)
-    (size_x, size_y) = window.get_size()
+    (size_x, size_y) = display.get_size()
     sprite.scale_x = size_x/float(sprite.width)
     sprite.scale_y = size_y/float(sprite.height)
-    play(load_song('assets/songs/OOT_AdultLink_Scream1.wav'))
+    play(load_songs(current_window.music))
 
-    @window.event
+    @display.event
     def on_draw():
         sprite.draw()
     pyglet.app.run()
 
-main()
+if __name__ == "__main__":
+  try:
+    main()
+  except KeyboardInterrupt:
+    # The user asked the program to exit
+    sys.exit(1)
+  except IOError, e:
+    # When this program is used in a shell pipeline and an earlier program in
+    # the pipeline is terminated, we'll receive an EPIPE error.  This is normal
+    # and just an indication that we should exit after processing whatever
+    # input we've received -- we don't consume standard input so we can just
+    # exit cleanly in that case.
+    if e.errno != errno.EPIPE:
+      raise
+
+    # We still exit with a non-zero exit code though in order to propagate the
+    # error code of the earlier process that was terminated.
+    sys.exit(1)
+  sys.exit(0)
