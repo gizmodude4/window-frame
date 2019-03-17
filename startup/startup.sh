17ed0e3bda3a3b4bc9ab859b2590e2eb6d077d26
Colby Sites <colby.sites@gmail.com>
2019-03-16 23:40:35 -0500

    Adding to the startup file.

--- startup/startup.sh
+++ startup/startup.sh
@@ -2,7 +2,11 @@
 xset s noblank
 xset s off
 xset -dpms
+if [ ! -L "/var/www/html/main.html" ]
+then
+  ln -s /home/pi/window-frame/js/site/main.html /var/www/html/main.html
+fi
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 node ~/window-frame/js/node/server.js --config ~/window-frame/js/node/playlist.json &
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost:8000 &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/main.html &
