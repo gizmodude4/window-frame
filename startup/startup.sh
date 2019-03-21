382dd8368a0dd15a56f752be8d1d76cd85ebf838
Colby Sites <colby.sites@gmail.com>
2019-03-20 23:14:36 -0500

    Adding unclutter line.

--- startup/startup.sh
+++ startup/startup.sh
@@ -6,6 +6,7 @@ if [ ! -L "/var/www/html/main.html" ]
 then
   sudo cp -r /home/pi/window-frame/site/* /var/www/html/
 fi
+unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 node ~/window-frame/node/server.js --config ~/window-frame/node/playlist.json &
