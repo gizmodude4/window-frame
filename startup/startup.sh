3a746fd489211d2fdacf5bc756c8b91e3ee18afe
Colby Sites <colby.sites@gmail.com>
2019-03-24 00:37:43 -0500

    Adding service and modifying startup.

--- startup/startup.sh
+++ startup/startup.sh
@@ -5,10 +5,11 @@ xset -dpms
 if [ ! -L "/var/www/html/main.html" ]
 then
   sudo cp -r /home/pi/window-frame/site/* /var/www/html/
+  sudo cp /home/pi/startup/windowframe.service /etc/systemd/system
 fi
 unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-node ~/window-frame/node/server.js --config ~/window-frame/node/playlist.json &
+node /home/pi/window-frame/node/server.js --config /home/pi/window-frame/node/playlist.json &
 sleep 5
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/main.html &
