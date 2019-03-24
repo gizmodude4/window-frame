4966f041c5e0b05542df4692f09945520bd2cf20
Colby Sites <colby.sites@gmail.com>
2019-03-24 06:37:11 +0000

    Updating startup scripts.

--- startup/startup.sh
+++ startup/startup.sh
@@ -5,11 +5,15 @@ xset -dpms
 if [ ! -L "/var/www/html/main.html" ]
 then
   sudo cp -r /home/pi/window-frame/site/* /var/www/html/
-  sudo cp /home/pi/startup/windowframe.service /etc/systemd/system
 fi
-unclutter -idle 0.5 -root &
+if [ ! -L "/lib/systemd/system/windowframe.service" ]
+then
+  sudo cp /home/pi/window-frame/startup/windowframe.service /lib/systemd/system
+fi
+#unclutter -idle 0.5 -root &
+unclutter -display :0 -noevents -grab &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-node /home/pi/window-frame/node/server.js --config /home/pi/window-frame/node/playlist.json &
-sleep 5
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/main.html &
+node /home/pi/window-frame/node/server.js --config /home/pi/window-frame/node/playlist.json
+#sleep 5
+#/usr/bin/chromium-browser --start-maximized --noerrdialogs --disable-infobars --kiosk http://localhost/main.html &
