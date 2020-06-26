795d0223526598202d8fa02d2bd5264ec65b8621
Colby Sites <colby.sites@gmail.com>
2020-06-26 18:26:59 -0500

    Easy space.

--- site/js/AudioManager.js
+++ site/js/AudioManager.js
@@ -1,81 +1,81 @@
 'use strict';
-
-class AudioManager {
-	constructor(audioEffectCreator) {
-		this.audioEffectCreator = audioEffectCreator;
-		this.currentlyPlayingAudio = {};
-	}
-
-	playAudio(audioDefinition, initialEffects, volumeOverride, onEndCb, finishedCb) {
-		var self = this;
-		try {
-			getAudio(audioDefinition, initialEffects, audio => {
-				if (volumeOverride) {
-					audio.volume = 0;
-				}
-				audio.play();
-				self.currentlyPlayingAudio[audioDefinition.getLink()] = audio;
-				audio.on("end", () => audio.disconnect());
-				if (onEndCb) {
-					audio.on('end', onEndCb);
-				}
-				if (finishedCb) {
-					finishedCb();
-				}
-			});
-		} catch(e) {
-			console.log("Error playing song " + audioDefinition.getLink() + ". Skipping...")
-			console.log(e.message);
-			if (finishedCb) {
-				finishedCb();
-			}
-			onEndCb();
-		}
-	}
-
-	stopAllPlayingAudio(cb) {
-		var self = this;
-		Object.keys(self.currentlyPlayingAudio).forEach(audioLink => {
-			var audio = self.currentlyPlayingAudio[audioLink];
-			audio.off('end');
-			audio.on("end", () => audio.disconnect());
-			audio.stop();
-		});
-		self.currentlyPlayingAudio = {};
-		if (cb) {
-			cb();
-		}
-	}
-
-	setCurrentlyPlayingVolume(audioLink, volume) {
-		if (audioLink in this.currentlyPlayingAudio) {
-			this.currentlyPlayingAudio[audioLink].volume = volume;
-		}
-	}
-}
-
-function getAudio(audio, initialEffects, cb) {
-	var newAudio = new Pizzicato.Sound({ 
-		source: 'file',
-		options: { 
-			path: audio.getLink(), 
-			volume: audio.getVolume()/100,
-			release: audio.getFadeDuration()/1000,
-			attack: audio.getFadeDuration()/1000,
-			loop: audio.getLoop()
-		}
-		}, function (err) {
-			if (err) {
-				throw err;
-			}
-			audio.getAudioEffects().forEach(effect => {
-				newAudio.addEffect(effect);
-			});
-			if (initialEffects && initialEffects.length > 0) {
-				initialEffects.forEach(effect => newAudio.addEffect(effect));
-			}
-			cb(newAudio);
-		});
-}
-
-export default AudioManager;
+ 
+ class AudioManager {
+     constructor(audioEffectCreator) {
+         this.audioEffectCreator = audioEffectCreator;
+         this.currentlyPlayingAudio = {};
+     }
+ 
+     playAudio(audioDefinition, initialEffects, volumeOverride, onEndCb, finishedCb) {
+         var self = this;
+         try {
+             getAudio(audioDefinition, initialEffects, audio => {
+                 if (volumeOverride) {
+                     audio.volume = 0;
+                 }
+                 audio.play();
+                 self.currentlyPlayingAudio[audioDefinition.getLink()] = audio;
+                 audio.on("end", () => audio.disconnect());
+                 if (onEndCb) {
+                     audio.on('end', onEndCb);
+                 }
+                 if (finishedCb) {
+                     finishedCb();
+                 }
+             });
+         } catch(e) {
+             console.log("Error playing song " + audioDefinition.getLink() + ". Skipping...")
+             console.log(e.message);
+             if (finishedCb) {
+                 finishedCb();
+             }
+             onEndCb();
+         }
+     }
+ 
+     stopAllPlayingAudio(cb) {
+         var self = this;
+         Object.keys(self.currentlyPlayingAudio).forEach(audioLink => {
+             var audio = self.currentlyPlayingAudio[audioLink];
+             audio.off('end');
+             audio.on("end", () => audio.disconnect());
+             audio.stop();
+         });
+         self.currentlyPlayingAudio = {};
+         if (cb) {
+             cb();
+         }
+     }
+ 
+     setCurrentlyPlayingVolume(audioLink, volume) {
+         if (audioLink in this.currentlyPlayingAudio) {
+             this.currentlyPlayingAudio[audioLink].volume = volume;
+         }
+     }
+ }
+ 
+ function getAudio(audio, initialEffects, cb) {
+     var newAudio = new Pizzicato.Sound({ 
+         source: 'file',
+         options: { 
+             path: audio.getLink(), 
+             volume: audio.getVolume()/100,
+             release: audio.getFadeDuration()/1000,
+             attack: audio.getFadeDuration()/1000,
+             loop: audio.getLoop()
+         }
+         }, function (err) {
+             if (err) {
+                 throw err;
+             }
+             audio.getAudioEffects().forEach(effect => {
+                 newAudio.addEffect(effect);
+             });
+             if (initialEffects && initialEffects.length > 0) {
+                 initialEffects.forEach(effect => newAudio.addEffect(effect));
+             }
+             cb(newAudio);
+         });
+ }
+ 
+ export default AudioManager;
--- site/js/main.js
+++ site/js/main.js
@@ -1,184 +1,185 @@
 'use strict';
-
-import WindowFrame from './WindowFrame.js';
-import SceneAudio from './SceneAudio.js';
-import Scene from './Scene.js';
-import Atmosphere from './Atmosphere.js';
-import AudioEffect from './AudioEffect.js';
-import AudioEffectCreator from './AudioEffectCreator.js';
-import AudioManager from './AudioManager.js';
-
-var image = document.getElementById('background');
-var stream = document.getElementById('player');
-var sceneDisplay = document.getElementById('sceneDisplay');
-var songDisplay = document.getElementById('songDisplay');
-var lastKeyPressed;
-var lastKeyPressedTime;
-var longPressTime = 3000;
-var isChromium = window.chrome ? true : false;
-var streamError = 0;
-var currTime = -1;
-
-stream.addEventListener('error', () => {
-	streamError++;
-	console.error('Audio stream error, reloading...');
-	if (streamError < 5) {
-		stream.load();
-	}
-});
-
-setInterval(() => {
-	var newTime = stream.currentTime;
-	if (currTime == -1) {
-		currTime = newTime;
-	} else if (currTime == newTime) {
-		console.error('Audio stream not plaing, reloading...');
-		stream.load();
-	}
-}, 10000);
-
-var windowFrame;
-
-function getScenesListener() {
-	var returnConfig = JSON.parse(this.responseText)
-	var scenes = parseScenes(returnConfig);
-	var atmosphereAudioManager = new AudioManager(AudioEffectCreator);
-	windowFrame = new WindowFrame(scenes, returnConfig['switchType'], image, sceneDisplay,
-								  songDisplay, stream, atmosphereAudioManager, sendSocketMessage,
-								  displayMessage, isChromium);
-	windowFrame.showScene(scenes[0]);
-}
-
-function parseScenes(sceneConfigs) {
-	var scenes = [];
-	sceneConfigs['scenes'].forEach(function(sceneConfig) {
-		var atmosphere = [];
-		var songAudioEffects = toAudioEffects(sceneConfig['audioEffects']);
-		sceneConfig['atmosphere'].forEach(function(atmosphereConfig) {
-			var atmosphereAudio = [];
-			if (atmosphereConfig['audio'] && atmosphereConfig['audio'].length > 0) {
-				atmosphereConfig['audio'].forEach(audioConfig => {
-					atmosphereAudio.push(toSceneAudio(audioConfig));
-				})
-			}
-			atmosphere.push(new Atmosphere(atmosphereConfig['image'], atmosphereConfig['name'], atmosphereConfig['streamVolume'], atmosphereAudio))
-		});
-		scenes.push(new Scene(sceneConfig['id'], sceneConfig['stream'], sceneConfig['streamVolume'], songAudioEffects, atmosphere, sceneConfig['endTime']));
-	});
-	return scenes;
-}
-
-function toAudioEffects(audioEffectConfig) {
-	var audioEffects = [];
-	if (audioEffectConfig && audioEffectConfig.length > 0) {
-		audioEffectConfig.forEach(effectConfig => {
-			audioEffects.push(AudioEffectCreator.createEffect(new AudioEffect(effectConfig['effectType'], effectConfig['config'])));
-		});
-	}
-	return audioEffects;
-}
-
-function toSceneAudio(audioConfig) {
-	return new SceneAudio(audioConfig['link'],
-		audioConfig['volume'],
-		audioConfig['fadeDuration'],
-		audioConfig['loop'],
-		toAudioEffects(audioConfig['audioEffects']));
-}
-
-function receiveMetadata(metadata) {
-	displayMessage(songDisplay, metadata.data);
-}
-
-function handleAction(event) {
-	if (lastKeyPressed != event.key) {
-		lastKeyPressed = event.key;
-		lastKeyPressedTime = Date.now();
-	}
-	var action = undefined;
-	if (event.key) {
-		action = keyToAction(event.key);
-	} else if (event.data) {
-		action = event.data;
-	}
-	if (!windowFrame.getProcessing()) {
-		windowFrame.setProcessing();
-		switch(action) {
-			case 'switch_effect':
-				windowFrame.playNextAtmosphere(() => { windowFrame.clearProcessing(); });
-				break;
-			case 'switch_song':
-				windowFrame.playNextSong(() => { windowFrame.clearProcessing(); });
-				break;
-			case 'switch_scene':
-				windowFrame.showNextScene(() => { windowFrame.clearProcessing(); });
-				break;    
-			default:
-				console.log('Unknown socket message type: ' + action);
-				break;
-		}
-	} else {
-		console.log('Received action ' + action + ' during processing. Ignoring...');
-	}
-}
-
-function displayMessage(tag, text) {
-	if (text) {
-		tag.textContent = text;
-		tag.style.opacity = 1;
-		setTimeout(() => {
-			var fadeOut = setInterval(() => {
-				tag.style.opacity = tag.style.opacity - 0.01;
-				if (tag.style.opacity <= 0) {
-					clearInterval(fadeOut);
-				}
-			}, 10);
-		}, 5000);
-	}
-}
-
-var oReq = new XMLHttpRequest();
-oReq.addEventListener('load', getScenesListener);
-oReq.open('GET', 'http://localhost:8080/scenes');
-oReq.send();
-
-var socket = new WebSocket('ws://localhost:8080/scenes/updates');
-socket.onmessage = receiveMetadata;
-
-function sendSocketMessage(message) {
-	var retry = setInterval(function() {
-		if (socket.readyState == WebSocket.OPEN) {
-			socket.send(message);
-			clearInterval(retry);
-		}
-	}, 1000);
-}
-
-function keyToAction(key) {
-	switch(key) {
-		case 's':
-			return 'switch_scene';
-		case 'e':
-			return 'switch_effect';
-		case 'm':
-			return 'switch_song';
-		default:
-			return undefined;
-	}
-}
-
-function checkIfLongPress(event) {
-	var reload = false;
-	if (lastKeyPressed == event.key && (Date.now() - lastKeyPressedTime) > longPressTime) {
-		reload = true;
-	}
-
-	lastKeyPressedTime = null;
-	lastKeyPressed = null;
-	if (reload) {
-		location.reload(true);
-	}
-}
-
-document.addEventListener('keydown', handleAction);
-document.addEventListener('keyup', checkIfLongPress);
+ 
+ import WindowFrame from './WindowFrame.js';
+ import SceneAudio from './SceneAudio.js';
+ import Scene from './Scene.js';
+ import Atmosphere from './Atmosphere.js';
+ import AudioEffect from './AudioEffect.js';
+ import AudioEffectCreator from './AudioEffectCreator.js';
+ import AudioManager from './AudioManager.js';
+ 
+ var image = document.getElementById('background');
+ var stream = document.getElementById('player');
+ var sceneDisplay = document.getElementById('sceneDisplay');
+ var songDisplay = document.getElementById('songDisplay');
+ var lastKeyPressed;
+ var lastKeyPressedTime;
+ var longPressTime = 3000;
+ var isChromium = window.chrome ? true : false;
+ var streamError = 0;
+ var currTime = -1;
+ 
+ stream.addEventListener('error', () => {
+     streamError++;
+     console.error('Audio stream error, reloading...');
+     if (streamError < 5) {
+         stream.load();
+     }
+ });
+ 
+ setInterval(() => {
+     var newTime = stream.currentTime;
+     if (currTime == -1) {
+         currTime = newTime;
+     } else if (currTime == newTime) {
+         console.error('Audio stream not plaing, reloading...');
+         stream.load();
+     }
+ }, 10000);
+ 
+ var windowFrame;
+ 
+ function getScenesListener() {
+     var returnConfig = JSON.parse(this.responseText)
+     var scenes = parseScenes(returnConfig);
+     var atmosphereAudioManager = new AudioManager(AudioEffectCreator);
+     windowFrame = new WindowFrame(scenes, returnConfig['switchType'], image, sceneDisplay,
+                                   songDisplay, stream, atmosphereAudioManager, sendSocketMessage,
+                                   displayMessage, isChromium);
+     windowFrame.showScene(scenes[0]);
+ }
+ 
+ function parseScenes(sceneConfigs) {
+     var scenes = [];
+     sceneConfigs['scenes'].forEach(function(sceneConfig) {
+         var atmosphere = [];
+         var songAudioEffects = toAudioEffects(sceneConfig['audioEffects']);
+         sceneConfig['atmosphere'].forEach(function(atmosphereConfig) {
+             var atmosphereAudio = [];
+             if (atmosphereConfig['audio'] && atmosphereConfig['audio'].length > 0) {
+                 atmosphereConfig['audio'].forEach(audioConfig => {
+                     atmosphereAudio.push(toSceneAudio(audioConfig));
+                 })
+             }
+             atmosphere.push(new Atmosphere(atmosphereConfig['image'], atmosphereConfig['name'], atmosphereConfig['streamVolume'], atmosphereAudio))
+         });
+         scenes.push(new Scene(sceneConfig['id'], sceneConfig['stream'], sceneConfig['streamVolume'], songAudioEffects, atmosphere, sceneConfig['endTime']));
+     });
+     return scenes;
+ }
+ 
+ function toAudioEffects(audioEffectConfig) {
+     var audioEffects = [];
+     if (audioEffectConfig && audioEffectConfig.length > 0) {
+         audioEffectConfig.forEach(effectConfig => {
+             audioEffects.push(AudioEffectCreator.createEffect(new AudioEffect(effectConfig['effectType'], effectConfig['config'])));
+         });
+     }
+     return audioEffects;
+ }
+ 
+ function toSceneAudio(audioConfig) {
+     return new SceneAudio(audioConfig['link'],
+         audioConfig['volume'],
+         audioConfig['fadeDuration'],
+         audioConfig['loop'],
+         toAudioEffects(audioConfig['audioEffects']));
+ }
+ 
+ function receiveMetadata(metadata) {
+     displayMessage(songDisplay, metadata.data);
+ }
+ 
+ function handleAction(event) {
+     if (lastKeyPressed != event.key) {
+         lastKeyPressed = event.key;
+         lastKeyPressedTime = Date.now();
+     }
+     var action = undefined;
+     if (event.key) {
+         action = keyToAction(event.key);
+     } else if (event.data) {
+         action = event.data;
+     }
+     if (!windowFrame.getProcessing()) {
+         windowFrame.setProcessing();
+         switch(action) {
+             case 'switch_effect':
+                 windowFrame.playNextAtmosphere(() => { windowFrame.clearProcessing(); });
+                 break;
+             case 'switch_song':
+                 windowFrame.playNextSong(() => { windowFrame.clearProcessing(); });
+                 break;
+             case 'switch_scene':
+                 windowFrame.showNextScene(() => { windowFrame.clearProcessing(); });
+                 break;    
+             default:
+                 console.log('Unknown socket message type: ' + action);
+                 break;
+         }
+     } else {
+         console.log('Received action ' + action + ' during processing. Ignoring...');
+     }
+ }
+ 
+ function displayMessage(tag, text) {
+     if (text) {
+         tag.textContent = text;
+         tag.style.opacity = 1;
+         setTimeout(() => {
+             var fadeOut = setInterval(() => {
+                 tag.style.opacity = tag.style.opacity - 0.01;
+                 if (tag.style.opacity <= 0) {
+                     clearInterval(fadeOut);
+                 }
+             }, 10);
+         }, 5000);
+     }
+ }
+ 
+ var oReq = new XMLHttpRequest();
+ oReq.addEventListener('load', getScenesListener);
+ oReq.open('GET', 'http://localhost:8080/scenes');
+ oReq.send();
+ 
+ var socket = new WebSocket('ws://localhost:8080/scenes/updates');
+ socket.onmessage = receiveMetadata;
+ 
+ function sendSocketMessage(message) {
+     var retry = setInterval(function() {
+         if (socket.readyState == WebSocket.OPEN) {
+             socket.send(message);
+             clearInterval(retry);
+         }
+     }, 1000);
+ }
+ 
+ function keyToAction(key) {
+     switch(key) {
+         case 's':
+             return 'switch_scene';
+         case 'e':
+             return 'switch_effect';
+         case 'm':
+             return 'switch_song';
+         default:
+             return undefined;
+     }
+ }
+ 
+ function checkIfLongPress(event) {
+     var reload = false;
+     if (lastKeyPressed == event.key && (Date.now() - lastKeyPressedTime) > longPressTime) {
+         reload = true;
+     }
+ 
+     lastKeyPressedTime = null;
+     lastKeyPressed = null;
+     if (reload) {
+         location.reload(true);
+     }
+ }
+ 
+ document.addEventListener('keydown', handleAction);
+ document.addEventListener('keyup', checkIfLongPress);
+ 
