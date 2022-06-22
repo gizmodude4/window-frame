92ed33e08e8a1af1cea49129e1c5250eb337420d
Colby Sites <colby.sites@gmail.com>
2022-06-22 01:03:18 -0500

    Fix unreliable night/nightEnd times. (#9)

--- site/js/getSunInfo.js
+++ site/js/getSunInfo.js
@@ -1,3 +1,5 @@
+import { fromMilitaryTime, timeLerp } from './dateTimeUtils.js';
+
 export const getSunInfo = (curTime, pos) => {    
     let timeToUse;
     if (!pos || !pos.lat || !pos.lon || !pos.tz) {
@@ -6,12 +8,23 @@ export const getSunInfo = (curTime, pos) => {
             lon: -98.5795,
             tz: "America/Chicago"
         }
-        
         timeToUse = luxon.DateTime.now({hour: 0, minute: 0, second: 0, millisecond: 0}).toJSDate()
     } else {
         timeToUse = curTime.set({hour: 0, minute: 0, second: 0, millisecond: 0}).toJSDate()
     }
     const sunCalcTimes = SunCalc.getTimes(timeToUse, pos.lat, pos.lon);
+
+    // night and nightEnd are known to be unreliable at times at certain places on the globe.
+    // If we don't get a time for these fields, average the dates and best as we can
+    // https://github.com/mourner/suncalc/issues/117
+    if (isNaN(sunCalcTimes.night.getTime())) {
+        const nextToMidnight = fromMilitaryTime('23:59');
+        sunCalcTimes.night = timeLerp(luxon.DateTime.fromJSDate(sunCalcTimes.dusk).setZone(pos.tz), nextToMidnight, 0.25).toJSDate();
+    }
+    if (isNaN(sunCalcTimes.nightEnd.getTime())) {
+        const midnight = fromMilitaryTime('00:00', pos.tz);
+        sunCalcTimes.nightEnd = timeLerp(midnight, luxon.DateTime.fromJSDate(sunCalcTimes.sunrise).setZone(pos.tz), 0.75).toJSDate();
+    }
     return {
         nightEnd: luxon.DateTime.fromJSDate(sunCalcTimes.nightEnd).setZone(pos.tz),
         sunrise: luxon.DateTime.fromJSDate(sunCalcTimes.sunrise).setZone(pos.tz),
